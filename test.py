9a7f89aefbe118f47aa4008d41cceb102b06c92a
rallerG <raller799@live.dk>
2019-02-13 12:07:04 +0100

    It runs and stops

--- test.py
+++ test.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python3
-from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_B, SpeedPercent, MoveTank
-from ev3dev2.sensor import INPUT_1
-from ev3dev2.sensor.lego import TouchSensor
+from ev3dev2.motor import LargeMotor, OUTPUT_C, OUTPUT_B, SpeedPercent, MoveTank
+from ev3dev2.sensor import INPUT_4
+# from ev3dev2.sensor.lego import TouchSensor
+from ev3dev2.sensor.lego import UltrasonicSensor
 from ev3dev2.led import Leds
 
 # TODO: Add code here
 leds = Leds()
 leds.set_color('LEFT', 'AMBER')
+
+tank_drive = MoveTank(OUTPUT_B, OUTPUT_C)
+ultra = UltrasonicSensor()
+ultra.mode = 'US-DIST-CM'
+
+while (ultra.value()/10) > 10:
+    tank_drive.on(50,50)
+
+tank_drive.off()
