e5ca2612bbb12f60709ed987106e661550b7ab27
benjaminsomm <37075712+benjaminsomm@users.noreply.github.com>
2019-03-20 11:14:13 +0100

    Add files via upload

---
+++ imagerec.py
@@ -0,0 +1,45 @@
+import numpy as np
+import cv2
+
+
+#cap = cv2.VideoCapture(0)
+#cap.set(cv2.CAP_PROP_FPS, 80)
+cap = cv2.imread('image.png')
+while(True):
+    # Capture frame-by-frame
+    #ret, frame = cap.read()
+
+    # Our operations on the frame come here
+    gray = cv2.cvtColor(cap, cv2.COLOR_BGR2GRAY)
+
+    gray = cv2.medianBlur(gray, 3)
+    rows = gray.shape[1]
+    circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1, rows / 10, param1=150, param2=60, minRadius=5, maxRadius=120)
+
+    if circles is not None:
+        circles = np.uint16(np.around(circles))
+        for i in circles[0, :]:
+
+            center = (i[0], i[1])
+            # circle center
+            cv2.circle(gray, center, 1, (0, 100, 100), 5)
+            # circle outline
+            radius = i[2]
+            cv2.circle(gray, center, radius, (255, 0, 255), 3)
+            cv2.line(gray, center, (300, 300), (255, 0, 0), 5)
+            print("x, y :", center)
+            print("radius :", radius)
+            print(len(circles))
+
+
+    # Display the resulting frame
+
+    cv2.imshow('frame', gray)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+
+cv2.destroyAllWindows()
