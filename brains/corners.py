b1bb53e6543cecdddd88da6c5b697d6539ded9e1
Thomas Mattsson <tmattsson0@gmail.com>
2019-06-06 12:41:56 +0200

    Update corners.py

--- brains/corners.py
+++ brains/corners.py
@@ -1,19 +1,41 @@
 import cv2
 import numpy as np
 
-img = cv2.imread("image2.jpg")
+img = cv2.imread("/Users/thomasmattsson/Documents/GitHub/CDIO/Test_images/ImageOfBane2.jpg")
 img = cv2.resize(img, (960, 540))
 
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+#
 
-corners = cv2.goodFeaturesToTrack(gray, 150, 0.01, 10)
-corners = np.int0(corners)
+boundaries = [
+    ([36, 30, 30], [86, 255,255]),
+    # ([86, 0, 0], [255, 0, 0])
+]
 
-for corner in corners:
-    x, y = corner.ravel()
-    cv2.circle(img, (x, y), 3, (0, 255, 0), -1)
+hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-cv2.imshow("img", img)
+# loop over the boundaries
+for (lower, upper) in boundaries:
+    # create NumPy arrays from the boundaries
+    lower = np.array(lower, dtype="uint8")
+    upper = np.array(upper, dtype="uint8")
+
+    # find the colors within the specified boundaries and apply
+    # the mask
+    mask = cv2.inRange(hsv, lower, upper)
+    output = cv2.bitwise_and(hsv, hsv, mask=mask)
+
+    # show the images
+    cv2.imshow("images", np.hstack([img, output]))
+
+#h, s, v = cv2.split(mask)
+
+cv2.imshow("gray-image",mask)
+
+coord = cv2.findNonZero(mask)
+
+print(coord)
+
+
+#cv2.imshow("img", img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
