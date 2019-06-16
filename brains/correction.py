238b15a4b9c141b0b8d22f0727d0924d19c4c650
Patrick Marcell Madsen <s175209@student.dtu.dk>
2019-06-16 20:05:04 +0200

    Update correction.py

--- brains/correction.py
+++ brains/correction.py
@@ -2,21 +2,27 @@ import math
 from model import point
 cam_height = 162.2
 robot_height = 15.5
-scale = 162.2 / 15.5
+ball_height = 4
+cam_robot_scale = cam_height / robot_height
+cam_ball_scale = cam_height / ball_height
 
 
 def point_correction(cam_cen, bl_point):
-    x_correction = round((cam_cen.x - bl_point.x) / scale)
-    y_correction = round((cam_cen.y - bl_point.y) / scale)
+    x_correction = round((cam_cen.x - bl_point.x) / cam_robot_scale)
+    y_correction = round((cam_cen.y - bl_point.y) / cam_robot_scale)
     px = bl_point.x + x_correction
     py = bl_point.y + y_correction
     p = point.Point(px, py)
     return p
 
 
-def ball_cen_correction():
-
-    return
+def ball_cen_correction(cam_cen, ball_cen):
+    x_correction = round((cam_cen.x - ball_cen.x) / cam_ball_scale)
+    y_correction = round((cam_cen.y - ball_cen.y) / cam_ball_scale)
+    px = round(ball_cen.x + x_correction / 2)
+    py = round(ball_cen.y + y_correction / 2)
+    p = point.Point(px, py)
+    return p
 
 
 # def main():
