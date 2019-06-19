fdeb10c8b1f05a2c1236c95e3f5c6ded583e0ad5
Søren <s175202@student.dtu.dk>
2019-06-19 14:54:56 +0200

    Bigger distance on corner ball

--- brains/waypoint.py
+++ brains/waypoint.py
@@ -62,7 +62,7 @@ def waypoints(endPoint):
     closestToBall_index = 0
     track = singleton.Singleton.track
     danger = round(track.pixelConversion * 15)
-    cornerSafePointX = round(track.pixelConversion * 10)
+    cornerSafePointX = round(track.pixelConversion * 20)
     cornerSafePointY = round(track.pixelConversion * 45)
     sideSafePoint = round(track.pixelConversion * 32)
     line_length = round(track.pixelConversion * 22)
