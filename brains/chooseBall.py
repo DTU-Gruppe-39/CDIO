ec8271673113de824611f951df6309caf6fa991a
Patrick Marcell Madsen <s175209@student.dtu.dk>
2019-06-20 09:12:46 +0200

    Update chooseBall.py

--- brains/chooseBall.py
+++ brains/chooseBall.py
@@ -139,7 +139,7 @@ def findSecondBestBall(balls):
         if is_ball_in_obstacle(chosen_ball):
             balls_in_obstacle = 0
             ball_dist.pop(0)
-            for i in range(len(ball_dist)):
+            for i in range(len(ball_dist), -1,  -1):
                 if is_ball_in_obstacle(ball_dist[i][1]):
                     ball_dist.pop(i)
                     balls_in_obstacle += 1
