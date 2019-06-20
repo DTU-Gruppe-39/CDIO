3baad15e15c21cf2677edb7823524542a681d08f
rallerG <raller799@live.dk>
2019-06-20 13:24:30 +0200

    .

--- brains/routeController.py
+++ brains/routeController.py
@@ -176,7 +176,7 @@ def goForGoal(expectedNumberOfBallsLeft):
                             # numberOfTriesToAlign = 0
                     else:
                         moreBallsThanExpected()
-                        robotController.createCommandTank(20, 20, 690)
+                        robotController.createCommandTank(20, 20, 710)
                         robotController.createCommandDeliver()
                         robotController.createCommandTank(-50, -50, 460)
                         print("\n\n\033[1;32m" + "Balls delivered to goal" + "\033[0m")
